f67cf173fdba19eab389348eda414cb7815b9295
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-12 15:16:42 +0900

    Google Fonts 패키지 추가 및 메인 화면에 데모 텍스트 구현

--- assets/lib/main.dart
+++ assets/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(body: Center(child: Text('Hello World'))),
+      home: Scaffold(
+          appBar: AppBar(title: Text('Google Fonts 데모')),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('헬로 월드!', style: TextStyle(fontSize: 24)),
+              Text('헬로 월드!',
+                  style: GoogleFonts.nanumPenScript(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.blue)),
+            ],
+          ))),
     );
   }
 }
