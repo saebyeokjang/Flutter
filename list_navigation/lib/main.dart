d8d5ff2f62a8637053395ddc6e993f474632761f
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-13 13:28:40 +0900

    MyListView 클래스를 새 파일로 분리

--- list_navigation/lib/main.dart
+++ list_navigation/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'my_list_view.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,46 +19,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyListView(),
-    );
-  }
-}
-
-class ListDataItems {
-  final List<String> monthItems = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ];
-}
-
-class MyListView extends StatelessWidget {
-  const MyListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final ListDataItems item = ListDataItems();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ListView'),
-      ),
-      body: ListView.builder(
-        itemCount: item.monthItems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(item.monthItems[index]),
-          );
-        },
-      ),
     );
   }
 }
---
+++ list_navigation/lib/my_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ListDataItems {
+  final List<String> monthItems = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+}
+
+class MyListView extends StatelessWidget {
+  const MyListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ListDataItems item = ListDataItems();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView'),
+      ),
+      body: ListView.builder(
+        itemCount: item.monthItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(item.monthItems[index]),
+          );
+        },
+      ),
+    );
+  }
+}
