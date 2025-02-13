a1f836edfd8f33951695a3f6e1114ec72da2867d
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-13 15:22:45 +0900

    아이템 터치 이벤트 추가 및 스낵바 표시 기능 구현

--- list_navigation/lib/my_grid_view.dart
+++ list_navigation/lib/my_grid_view.dart
@@ -27,14 +27,29 @@ class MyGridView extends StatelessWidget {
           itemCount: 30, // 총 30개의 아이템을 생성합니다.
           itemBuilder: (context, index) {
             // 각 아이템을 생성하는 함수입니다.
-            return Container(
-              // 각 아이템은 Container 위젯으로 구성됩니다.
-              color: Colors.blue[100 *
-                  (index %
-                      9)], // 아이템의 배경색을 설정합니다.  index에 따라 파란색 계열의 다른 색상이 적용됩니다. (9가지 색상 반복)
-              child: Center(
-                // Container 내부에 내용을 중앙 정렬합니다.
-                child: Text('Item $index'), // 'Item'과 함께 아이템의 인덱스를 텍스트로 표시합니다.
+            return InkWell(
+              // InkWell 위젯을 사용하여 터치 이벤트를 처리합니다.
+              onTap: () {
+                // 아이템을 터치했을 때의 동작을 정의합니다.
+                debugPrint(
+                    'Item $index tapped'); // 콘솔에 'Item $index tapped'를 출력합니다.
+                // ScaffoldMessenger를 사용하여 스낵바를 표시합니다.
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Item $index tapped'),
+                  ),
+                );
+              },
+              child: Container(
+                // 각 아이템은 Container 위젯으로 구성됩니다.
+                color: Colors.blue[100 *
+                    (index %
+                        9)], // 아이템의 배경색을 설정합니다.  index에 따라 파란색 계열의 다른 색상이 적용됩니다. (9가지 색상 반복)
+                child: Center(
+                  // Container 내부에 내용을 중앙 정렬합니다.
+                  child:
+                      Text('Item $index'), // 'Item'과 함께 아이템의 인덱스를 텍스트로 표시합니다.
+                ),
               ),
             );
           },
