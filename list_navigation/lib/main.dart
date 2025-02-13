5f28d26236d775faa1f87ee8c55299858c7c9741
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-13 13:16:07 +0900

    홈 화면을 MyListView로 변경하고, 월별 항목을 표시하는 ListView 추가

--- list_navigation/lib/main.dart
+++ list_navigation/lib/main.dart
@@ -16,32 +16,46 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyListView(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class ListDataItems {
+  final List<String> monthItems = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyListView extends StatelessWidget {
+  const MyListView({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final ListDataItems item = ListDataItems();
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('ListView'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: ListView.builder(
+        itemCount: item.monthItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(item.monthItems[index]),
+          );
+        },
       ),
     );
   }
