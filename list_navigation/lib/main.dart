7c65187ca7ad1fbc426021361748f07c5db4167e
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-13 15:34:50 +0900

    Navigator 1.0 사용: ListTile을 사용하여 항목 클릭 시 상세 페이지로 이동하는 기능 추가

--- list_navigation/lib/main.dart
+++ list_navigation/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'my_grid_view.dart';
+import 'my_list_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyGridView(),
+      home: const MyListView(),
     );
   }
 }
--- list_navigation/lib/my_list_view.dart
+++ list_navigation/lib/my_list_view.dart
@@ -30,16 +30,37 @@ class MyListView extends StatelessWidget {
         title: const Text('ListView'),
       ),
       body: ListView.builder(
-        scrollDirection: Axis.horizontal,
         itemCount: item.monthItems.length,
         itemBuilder: (context, index) {
-          return Row(
-            children: [
-              Text(item.monthItems[index]),
-              const SizedBox(width: 10),
-            ],
-          );
+          return ListTile(
+              title: Text(item.monthItems[index]),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            MyDetails(item.monthItems[index])));
+              });
         },
+        itemExtent: 50,
+      ),
+    );
+  }
+}
+
+class MyDetails extends StatelessWidget {
+  final String month;
+  const MyDetails(this.month, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(month),
+      ),
+      body: Center(
+        child: Text('This is the details page for $month'),
       ),
     );
   }
