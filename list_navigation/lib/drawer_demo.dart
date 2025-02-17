7a59df973164be078f0a1228c54460c7ecfed5b3
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-17 10:28:09 +0900

    드로어 위젯 및 데모 페이지 추가

--- list_navigation/lib/drawer_demo.dart
+++ list_navigation/lib/drawer_demo.dart
@@ -11,6 +11,90 @@ class DrawerDemo extends StatelessWidget {
         ),
         body: Center(
           child: Text('Drawer Demo'),
-        ));
+        ),
+        drawer: MyDrawerWidget());
+  }
+}
+
+class MyDrawerWidget extends StatelessWidget {
+  const MyDrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(
+      children: [
+        const DrawerHeader(child: Icon(Icons.home, size: 35)),
+        ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Drawer Item #1'),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => DemoPageOne()));
+            }),
+        ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Drawer Item #2'),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => DemoPageTwo()));
+            }),
+        ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Drawer Item #3'),
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => DemoPageThree()));
+            }),
+      ],
+    ));
+  }
+}
+
+class DemoPageOne extends StatelessWidget {
+  const DemoPageOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Demo Page One'),
+        ),
+        body: Center(
+          child: Text('Demo Page One'),
+        ),
+        drawer: MyDrawerWidget());
+  }
+}
+
+class DemoPageTwo extends StatelessWidget {
+  const DemoPageTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Demo Page Two'),
+        ),
+        body: Center(
+          child: Text('Demo Page Two'),
+        ),
+        drawer: MyDrawerWidget());
+  }
+}
+
+class DemoPageThree extends StatelessWidget {
+  const DemoPageThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Demo Page Three'),
+        ),
+        body: Center(
+          child: Text('Demo Page Three'),
+        ),
+        drawer: MyDrawerWidget());
   }
 }
