bf6e061ea5ca88470f6b40ab41f636c1b5240e17
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-17 13:46:33 +0900

    JSON 파일에서 월 목록을 비동기적으로 로드하도록 수정

--- data-json/my_data_demo/lib/main.dart
+++ data-json/my_data_demo/lib/main.dart
@@ -1,30 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyData {
-  final List<String> items = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  late Future<List<String>> futureData;
 
-  final MyData data = MyData();
+  @override
+  void initState() {
+    super.initState();
+    futureData = _loadMonths();
+  }
+
+  Future<List<String>> _loadMonths() async {
+    String jsonString = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/example.json');
+    // json 파싱된 결과를 List<String>으로 변환
+    return jsonDecode(jsonString).values.cast<String>().toList();
+  }
 
   // This widget is the root of your application.
   @override
@@ -36,10 +40,20 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text('My Data App')),
-        body: ListView.builder(
-          itemCount: data.items.length,
-          itemBuilder: (context, index) {
-            return ListTile(title: Text(MyData().items[index]));
+        body: FutureBuilder<List<String>>(
+          future: futureData,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              // 미래 데이터에 대해 값이 없는 경우 (1: 값이 안넘어온 경우, 2: 로딩 중, 3: 에러)
+              return const Center(child: CircularProgressIndicator());
+            }
+            final data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(data[index]));
+              },
+            );
           },
         ),
       ),
